fc376928947f201dce8f6fe2bf1b2eec8937db4b
KhanhHoang111 <43487666+KhanhHoang111@users.noreply.github.com>
2024-09-28 23:48:18 +0700

    Update config.py

--- azure-migration-udacity-prj3-techconf-main/web/config.py
+++ azure-migration-udacity-prj3-techconf-main/web/config.py
@@ -11,11 +11,11 @@ class BaseConfig:
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    #SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
     SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/TfPtMQ1JB+DLJBbMS36TwWGN7wL3WLcm+ASbIlfJ2w=' #TODO: Update value
-    #SERVICE_BUS_QUEUE_NAME ='notificationqueue'
-    #ADMIN_EMAIL_ADDRESS= "info@techconf.co"
-    #SENDGRID_API_KEY = 'get_from_send_grid' #Configuration not required, required SendGrid Account
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS= "info@techconf.co"
+    SENDGRID_API_KEY = 'get_from_send_grid' #Configuration not required, required SendGrid Account
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
