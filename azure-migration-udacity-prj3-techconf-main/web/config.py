3fca29344a23ae22fc645d05c74f4ab5b3933bf5
KhanhHoang111 <43487666+KhanhHoang111@users.noreply.github.com>
2024-09-28 22:40:30 +0700

    Add files via upload
    commit 4

---
+++ azure-migration-udacity-prj3-techconf-main/web/config.py
@@ -0,0 +1,25 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="techconf-k.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="azureuser" #TODO: Update value
+    POSTGRES_PW="Kkhanhss#154"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    #SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/TfPtMQ1JB+DLJBbMS36TwWGN7wL3WLcm+ASbIlfJ2w=' #TODO: Update value
+    #SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    #ADMIN_EMAIL_ADDRESS= "info@techconf.co"
+    #SENDGRID_API_KEY = 'get_from_send_grid' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
